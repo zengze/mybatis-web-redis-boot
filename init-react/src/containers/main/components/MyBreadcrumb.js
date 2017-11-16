3b3980c8d04eaa58fe4ddf8d2c949379e8542971
zengze <zengze@smart>
2017-11-16 11:15:20 +0800

    调整面包屑

--- init-react/src/containers/main/components/MyBreadcrumb.js
+++ init-react/src/containers/main/components/MyBreadcrumb.js
@@ -1,21 +1,24 @@
 import { Link } from 'react-router';
 import React from 'react';
+import {Breadcrumb} from 'antd';
 
 const MyBreadcrumb = ({routes}) => {
-    return (
-            // <Breadcrumb routes={routes} params={params} style={{margin: '12px 0'}} />
-            <div  className="breadcrumbName">
-                {
-                    routes.map((item , i) => {
-                        if(item.breadcrumbName)
-                        return routes.length === (i+1) ? 
-                        <label style={{fontWeight:'bold'}} key={i}>{` ${item.breadcrumbName} `}</label>
-                        :
-                        <label  key={i}><Link to={item.path}>{` ${item.breadcrumbName} ${ routes.length === 0 ? '' : '/'}`}</Link></label>
-                    })
-                }
-            </div>
-        );
+
+  return (
+    <div style={{ paddingLeft: 20, paddingTop: 20 }}>
+      <Breadcrumb>
+        {
+          routes.map((item , i) => {
+            if(i < routes.length - 1) {
+              return <Breadcrumb.Item key={i}><a href={item.path}>{item.breadcrumbName}</a></Breadcrumb.Item>
+            } else {
+              return <Breadcrumb.Item key={i}>{item.breadcrumbName}</Breadcrumb.Item>
+            }
+          })
+        }
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default MyBreadcrumb;
